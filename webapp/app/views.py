6a7d20c314f6c835e037fa8712369d79e2638412
Christopher A. Mosher <cmosher01@gmail.com>
2016-11-05 02:28:47 -0400

    begin changing directories to nested; add data thru 1980

--- webapp/app/views.py
+++ webapp/app/views.py
@@ -5,3 +5,7 @@ import flask
 @application.route('/show/<id>')
 def _show(id):
     return flask.render_template('show.html', show=database.read_show(application.root_path, id))
+
+@application.route('/shows/<id>')
+def _shows(id):
+    return flask.render_template('shows.html', show=database.read_shows(application.root_path, id))
